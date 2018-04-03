398839d377c07f34fa86bd3f13a9f530220b60b0
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-04-03 23:35:17 +0300

    add basic server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+const mongoose = require('mongoose')
+
+const router = require('./router')
+
+mongoose.connect('mongodb://localhost/auth')
+
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+router(app)
+
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+
+console.log('Server listening on:', port)
