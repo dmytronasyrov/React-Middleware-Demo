ccd76e4e66ed87416b7906fdd6a89de7207596de
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-04-07 11:30:00 +0300

    add auth HOC

--- client/src/index.js
+++ client/src/index.js
@@ -7,23 +7,33 @@ import reduxThunk from 'redux-thunk'
 
 import reducers from './reducers';
 import Async from './middlewares/async'
+import { AUTH_USER } from './actions/types'
 
+import RequireAuth from './components/HOC/RequireAuth'
 import App from './components/App';
+import Welcome from './components/Welcome'
 import SignUp from './components/auth/SignUp'
 import SignIn from './components/auth/SignIn'
 import SignOut from './components/auth/SignOut'
 import UserList from './components/UserList'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk, Async)(createStore);
+const store = createStoreWithMiddleware(reducers)
+const token = localStorage.getItem('token')
+
+if (token) {
+  store.dispatch({ type: AUTH_USER })
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={ store }>
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
+        <IndexRoute component={ Welcome }/>
         <Route path="signup" component={ SignUp }/>
         <Route path="signin" component={ SignIn }/>
         <Route path="signout" component={ SignOut }/>
-        <Route path="users" component={ UserList }/>
+        <Route path="users" component={ RequireAuth(UserList) }/>
       </Route>
     </Router>
   </Provider>
